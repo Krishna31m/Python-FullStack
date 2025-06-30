ebf6b3cc00170483236227d03800036fa169891c
Krishna31m <119494212+Krishna31m@users.noreply.github.com>
2025-06-30 14:08:51 +0530

    Add files via upload
    The Todo List project is a simple, user-friendly web application designed to help users efficiently manage their daily tasks. Built with Django as the backend framework and styled using Tailwind CSS for a modern, responsive UI, this app allows users to:
    
    Create new todo items with a title and detailed description.
    
    View a list of all their todos in a clean, organized interface.
    
    Click on individual todo items to see full details on a dedicated page.
    
    Update existing todos to keep their task list current.
    
    Delete completed or unwanted tasks with ease.

---
+++ TODO/todo_list/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView, TodoDetailView
+
+urlpatterns = [
+    path("", TodoListView.as_view(), name='todo_list'),
+    path("create/", TodoCreateView.as_view(), name='todo_create'),
+    path("update/<int:pk>/", TodoUpdateView.as_view(), name='todo_update'),
+    path("delete/<int:pk>/", TodoDeleteView.as_view(), name='todo_delete'),
+    path('todo/<int:pk>/', TodoDetailView.as_view(), name='todo_detail'),
+
+]
